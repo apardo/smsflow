16112b2f01850999062d4afe9c3d684ce0bcc098
Antonio Pardo <apardo@alabs.es>
2011-06-07 09:28:14 +0200

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smsflow::Application.config.secret_token = '4b8d7149a8e93449b8faeaa0401fbc05d3963a1a0e57143227093aa8df0cf269c8a2aecd4f671daba66cec423c8abfcfe59489343032fc4bacc757e31137cdbd'
